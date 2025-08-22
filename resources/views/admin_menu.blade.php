36c19cddec08668c9060438957d452ea25e9ab5f
Administrator <Administrator@ADMIN.domenfo.com>
2025-08-22 14:20:40 +0300

    Незначительная правка шаблонов модуля "Архив"

--- resources/views/admin_menu.blade.php
+++ resources/views/admin_menu.blade.php
@@ -2,14 +2,21 @@
     <div class="top-category text-center no-border-left">
         <h3><a href="{{ route('delo-out')}}">Дело</a></h3>
         <span class="icon flaticon-contact"></span>
-        <p><span class="number"></span> <span>Модуль готов к работе</span></p>
+        <p><span class="number"></span> <span>Регистрация корреспонденции</span></p>
     </div>
 </div>
 <div class="col-md-2">
     <div class="top-category text-center">
-        <h3><a href="{{ route('communal-admin')}}">Коммуналка</a></h3>
+        <h3><a href="#">Коммуналка</a></h3>
         <span class="icon flaticon-accounting"></span>
         <p><span class="number"></span> <span>Модуль в разработке</span></p>
+    </div>
+</div>
+<div class="col-md-2">
+    <div class="top-category text-center">
+        <h3><a href="{{ route('communal-admin')}}">Архив</a></h3>
+        <span class="icon flaticon-accounting"></span>
+        <p><span class="number"></span> <span>Коммуналка 2018-2025</span></p>
     </div>
 </div>
 <div class="col-md-2">
@@ -22,20 +29,13 @@
 <div class="col-md-2">
     <div class="top-category text-center">
         <h3><a href="#">Архив ОФС</a></h3>
-        <span class="icon flaticon-accounting"></span>
-        <p><span class="number"></span> <span>Модуль в разработке</span></p>
-    </div>
-</div>
-<div class="col-md-2">
-    <div class="top-category text-center">
-        <h3><a href="#">Администратор</a></h3>
         <span class="icon flaticon-contact"></span>
         <p><span class="number"></span> <span>Модуль в разработке</span></p>
     </div>
 </div>
 <div class="col-md-2">
     <div class="top-category text-center">
-        <h3><a href="#">Разработка</a></h3>
+        <h3><a href="#">Администратор</a></h3>
         <span class="icon flaticon-contact"></span>
         <p><span class="number"></span> <span>Open position</span></p>
     </div>
