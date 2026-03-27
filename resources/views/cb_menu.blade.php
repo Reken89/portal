15d58028a1ac53406d848db8ce05db51133f951b
Administrator <Administrator@ADMIN.domenfo.com>
2026-03-27 09:07:47 +0300

    Разработка нового модуля "ОФС 2026". Визуальная правка шаблона

--- resources/views/cb_menu.blade.php
+++ resources/views/cb_menu.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-2">
     <div class="top-category text-center no-border-left">
-        <h3><a href="{{ route('ofs-user')}}">ОФС 2026</a></h3>
+        <h3><a href="{{ route('ofs26-user')}}">ОФС 2026</a></h3>
         <span class="icon flaticon-accounting"></span>
         <p><span class="number"></span> <span>ОФС 2026</span></p>
     </div>
@@ -14,7 +14,7 @@
 </div>
 <div class="col-md-2">
     <div class="top-category text-center">
-        <h3><a href="{{ route('ofs26-user')}}">Модуль №3</a></h3>
+        <h3><a href="#">Модуль №3</a></h3>
         <span class="icon flaticon-contact"></span>
         <p><span class="number"></span> <span>Модуль закрыт</span></p>
     </div>
