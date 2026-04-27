593878ed10afcaa64f957d199813abd010f03491
Administrator <Administrator@ADMIN.domenfo.com>
2026-04-27 11:33:48 +0300

    Сознание модуля "Прогноз коммунальных услуг". Шаблоны, контроллер, бизнес логика

--- resources/views/forecast/admin/tables/table_2.blade.php
+++ resources/views/forecast/admin/tables/table_2.blade.php
@@ -1,5 +1,40 @@
 @php
-    var_dump($info['info']);
+    //var_dump($info['info']);
 @endphp
-<p>Таблица ошибок</p>
+<table class="table2">
+    <thead>
+        <tr>
+            <th>Январь (2026)</th>
+            <th>Февраль (2026)</th>
+            <th>Март (2026)</th>
+            <th>Апрель (2026)</th>
+            <th>Май (2026)</th>
+            <th>Июнь (2026)</th>
+            <th>Июль (2025)</th>
+            <th>Август (2025)</th>
+            <th>Сентябрь (2025)</th>
+            <th>Октябрь (2025)</th>
+            <th>Ноябрь (2025)</th>
+            <th>Декабрь (2025)</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            @foreach($info['info'] as $monthNumber => $isActive)
+                @if($loop->iteration > 6)
+                    @break
+                @endif
+                <td style="background-color: {{ $isActive ? 'green' : 'red' }};">
+                    {{ $monthNumber }}
+                </td>
+            @endforeach
+            <td style="background-color: green;">7</td>
+            <td style="background-color: green;">8</td>
+            <td style="background-color: green;">9</td>
+            <td style="background-color: green;">10</td>
+            <td style="background-color: green;">11</td>
+            <td style="background-color: green;">12</td>
+        </tr>
+    </tbody>
+</table>
 
